9940a645eb15c5c3776770ba65f1cc13d391d405
d-krng <krng.daniel@gmail.com>
2024-01-07 21:12:49 +0100

    finish JDBC Repository test

--- src/test/java/com/dk/customer/CustomerJDBCDataAccessServiceTest.java
+++ src/test/java/com/dk/customer/CustomerJDBCDataAccessServiceTest.java
@@ -2,11 +2,14 @@ package com.dk.customer;
 
 import com.dk.AbstractTestcontainers;
 import org.assertj.core.api.Assertions;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.relational.core.sql.In;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainers {
 
@@ -61,18 +64,77 @@ class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainers {
     }
 
     @Test
+    void willReturnEmptyWhenSelectCustomerById() {
+        //Given
+        int id = -1;
+        //When
+        var actual = underTest.selectCustomerById(id);
+        //Then
+        Assertions.assertThat(actual).isEmpty();
+    }
+
+    @Test
     void insertCustomer() {
         //Given
+        Customer testCustomer = createTestCustomer();
 
         //When
+        underTest.insertCustomer(testCustomer);
+
+        String email = testCustomer.getEmail();
+        Integer id = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(email))
+                .findFirst()
+                .map(c -> c.getId())
+                .orElseThrow();
+
+        Optional<Customer> actual = underTest.selectCustomerById(id);
 
         //Then
+        Assertions.assertThat(actual).hasValueSatisfying( c -> {
+            Assertions.assertThat(c.getId()).isEqualTo(id);
+            Assertions.assertThat(c.getEmail()).isEqualTo(testCustomer.getEmail());
+            Assertions.assertThat(c.getName()).isEqualTo(testCustomer.getName());
+        });
+
     }
 
     @Test
     void existsCustomerWithEmail() {
         //Given
+        Customer testCustomer = createTestCustomer();
 
+        //When
+        underTest.insertCustomer(testCustomer);
+
+        String email = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(testCustomer.getEmail()))
+                .findFirst()
+                .map(c -> c.getEmail())
+                .orElseThrow();
+        Integer id = underTest.selectAllCustomers()
+                .stream()
+                .filter(c -> c.getEmail().equals(testCustomer.getEmail()))
+                .findFirst()
+                .map(c -> c.getId())
+                .orElseThrow();
+
+        boolean actualReturn =  underTest.existsCustomerWithEmail(email);
+
+        //Then
+        Assertions.assertThat(actualReturn).isTrue();
+        Assertions.assertThat(underTest.selectCustomerById(id)).hasValueSatisfying( c -> {
+            Assertions.assertThat(c.getEmail()).isEqualTo(email);
+        });
+
+    }
+
+    @Test
+    void willReturnEmptyWhenIdIsNotPresent() {
+        //Given
+        int id = -1;
         //When
 
         //Then
@@ -80,28 +142,54 @@ class CustomerJDBCDataAccessServiceTest extends AbstractTestcontainers {
 
     @Test
     void deleteCustomerById() {
-        //Given
+        //Given maybe second test for thid method
+        Customer testCustomer = createTestCustomer();
 
         //When
-
+        underTest.insertCustomer(testCustomer);
+        Assertions.assertThat(underTest.selectAllCustomers()).isNotEmpty();
+        int id = underTest.selectAllCustomers()
+                        .stream()
+                                .filter(c -> c.getEmail().equals(testCustomer.getEmail()))
+                                        .findFirst()
+                                                .map(c -> c.getId())
+                                                        .orElseThrow();
+        underTest.deleteCustomerById(id);
+        Optional<Customer> actual = underTest.selectCustomerById(id);
         //Then
+        Assertions.assertThat(actual).isEmpty();
     }
 
     @Test
     void updateCustomer() {
         //Given
+        Customer testCustomer = createTestCustomer();
+        UUID unique = UUID.randomUUID();
+        String name = "test"+unique;
+        String email = "test@"+unique;
+        Integer age = 69;
 
         //When
+        underTest.insertCustomer(testCustomer);
+
+        testCustomer.setEmail(email);
+        underTest.updateCustomer(testCustomer);
+
 
         //Then
+        Assertions.assertThat(testCustomer.getEmail()).isEqualTo(email);
     }
 
     @Test
     void existsCustomerById() {
         //Given
-
+        Customer testCustomer = createTestCustomer();
         //When
+        underTest.insertCustomer(testCustomer);
+        Integer id = underTest.selectAllCustomers().stream().filter(c->c.getEmail().equals(testCustomer.getEmail())).findFirst().map(c->c.getId()).orElseThrow();
+        boolean existsCustomerWithId = underTest.existsCustomerById(id);
 
         //Then
+        Assertions.assertThat(existsCustomerWithId).isTrue();
     }
 }
