18e744ee31223b0b7bbb6444df8d3af3b2c45e0e
d-krng <krng.daniel@gmail.com>
2024-01-27 23:40:39 +0100

    CD workflow with controller

---
+++ backend/src/main/java/com/dk/PingPongController.java
@@ -0,0 +1,15 @@
+package com.dk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+    record PingPong(String result) {}
+
+    @RequestMapping(method = RequestMethod.GET, value = "/ping")
+    public PingPong getPingPong() {
+    return new PingPong("Pong");
+    }
+}
