77fab7c89ef4a73c6a90b9184ef72a28e45ac096
d-krng <krng.daniel@gmail.com>
2024-02-02 13:36:34 +0100

    Test implemnted step cd-backend

--- backend/src/main/java/com/dk/PingPongController.java
+++ backend/src/main/java/com/dk/PingPongController.java
@@ -10,6 +10,6 @@ public class PingPongController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/ping")
     public PingPong getPingPong() {
-    return new PingPong("PongPingPongPingPongPong");
+    return new PingPong("PongPingPongPingPongPongPing");
     }
 }
