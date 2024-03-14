08657e90aca7e952a93a57a0c16d2ed21e831c90
d-krng <krng.daniel@gmail.com>
2024-03-14 14:13:00 +0100

    Fix Deployment issue part 02

--- backend/src/main/java/com/dk/PingPongController.java
+++ backend/src/main/java/com/dk/PingPongController.java
@@ -12,6 +12,6 @@ public class PingPongController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/ping")
     public PingPong getPingPong() {
-    return new PingPong("Pongss: %s" .formatted(++COUNTER));
+    return new PingPong("Pongsss: %s" .formatted(++COUNTER));
     }
 }
